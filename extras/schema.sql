5c005e76045c2c79a4d1daafcf62bc9ce9cecc97
pepo viola <pepoviola@gmail.com>
2013-12-30 12:04:51 -0300

    change field type to text

--- extras/schema.sql
+++ extras/schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS `logs` (
+CREATE TABLE IF NOT EXISTS `log_table` (
 	`id` int(10) NOT NULL AUTO_INCREMENT,
 	`level` varchar(45) NOT NULL,
 	`message` text NOT NULL,
 	`timestamp` datetime NOT NULL,
 	`meta` varchar(255),
-	`meta_stack` varchar(5000),
+	`meta_stack` text NOT NULL,
 	`hostname` varchar(255),
 	PRIMARY KEY (`id`)
 );
